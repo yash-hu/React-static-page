8cb33e145cf397ac2cc8e248f3e6dddf7512dd03
Yash <chauhanyash4567@gmail.com>
2023-08-03 16:50:42 +0530

    Complete static react app

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import logo from "../logo.svg";
+
+export const NavBar = () => {
+  return (
+    <nav>
+      <img src={logo} alt="React logo" />
+      <h3>ReactFacts</h3>
+      <h4>React Project-1</h4>
+    </nav>
+  );
+};
